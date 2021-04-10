34ce3bbc2ae4c8eb7b126f74d6e66434bf713bfa
lisab1993 <brown.lisa999@gmail.com>
2021-04-09 21:42:35 -0700

    work done in class

--- src/async.js
+++ src/async.js
@@ -1,15 +1,32 @@
 // Async Utilities
 
-function parallel (tasks, callback) {
+//callback receives the argument
+function parallel(tasks, callback) {
+  let output = []
+  let count = 0
 
+  for (let i = 0; i < tasks.length; i++) {
+    let taskFunction = tasks[i]
+    
+    taskFunction(function (error, result) {
+      output[i] = result
+      count++
+      if (error) {
+        return callback(error, output)
+      }
+      if (count === 2) {
+        return callback(error, output)
+      }
+    })
+  }
 }
 
 // Try to define map using parallel
-function map (collection, iteratee, callback) {
+function map(collection, iteratee, callback) {
 
 }
 
-function waterfall (tasks, callback) {
+function waterfall(tasks, callback) {
 
 }
 
